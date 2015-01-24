f29dc612044c57d43d85cdedb7156eed3fee5ea6
Kamil <ant@ant-VirtualBox.(none)>
2015-01-24 15:37:39 +0100

    Add wiki and change main.cpp to button and led example

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@
 #include "./systemControl/GPIO/GPIO.h"
 
 int main(){
-	GPIO ledTest( 89 );
-	ledTest.setDirection( GPIO::OUT_DIRECTION );
+	GPIO led( 89 );
+	GPIO button( 46 );
+
+	led.setDirection( GPIO::OUT_DIRECTION );
+	button.setDirection( GPIO::IN_DIRECTION );
 
 	while( true ){
-		usleep( 1000000 );
-		ledTest.setValue( 0 );
-
-		usleep( 1000000 );
-		ledTest.setValue( 1 );
+		usleep( 10000 );
+		if( button.getValue() == 0 ){
+			led.setValue( 1 );
+		} else {
+			led.setValue( 0 );
+		}
 	}
 
 	return 0;
